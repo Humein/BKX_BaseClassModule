5f72c50668d348b48056b4753f9eed2d9e4355d3
Zhang Xin Xin <shou1wang@gmail.com>
2019-10-10 18:00:37 +0800

    更新描述

---
+++ Example/BaseClassModulesRepo/BKXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BKXAppDelegate.h
+//  BaseClassModulesRepo
+//
+//  Created by Zhang Xin Xin on 10/08/2019.
+//  Copyright (c) 2019 Zhang Xin Xin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BKXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/BaseClassModulesRepo/BKXViewController.h
@@ -0,0 +1,13 @@
+//
+//  BKXViewController.h
+//  BaseClassModulesRepo
+//
+//  Created by Zhang Xin Xin on 10/08/2019.
+//  Copyright (c) 2019 Zhang Xin Xin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BKXViewController : UIViewController
+
+@end
